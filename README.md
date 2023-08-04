26e725c657c55da3c780ef2c317d8834bb5784e4
GustaMantovani <mantovanig000@gmail.com>
2023-08-04 17:18:46 -0300

    frist commit

---
+++ README.md
@@ -0,0 +1,6 @@
+# PathTop
+
+Este é um script Python que permite a busca por processos iniciados por um determinado binário e exibe algumas métricas de uso de CPU e memória para cada processo encontrado.
+O script utiliza os módulos os, psutil e subprocess do Python. O módulo os é usado para listar os diretórios em /proc, o módulo psutil é usado para acessar dados de uso de CPU, memória e disco do processo.
+Além de ser útil para monitorar métricas de processos em desenvolvimento, este código pode ser usado para monitorar processos em produção, desde que se tenha acesso ao caminho do binário que gerou os processos que se deseja monitorar. Essa abordagem é conveniente, pois não é necessário instalar softwares adicionais ou configurar ambientes complexos para monitorar os processos.
+Para executar o script, basta salvar o código em um arquivo com a extensão .py e executar o comando python nome_do_arquivo.py no terminal Linux, depois insira o caminho para 
---
+++ pytop.py
@@ -0,0 +1,49 @@
+import os
+import psutil
+
+def listDirs():#listar os diretórios em /proc
+    #array que armazena os diretórios de /proc
+    sysDirs = [f for f in os.listdir('/proc') if f.isdigit()] #função da biblioteca os que lista o diretorio do caminho que recebe em seu parâmetro
+    return sysDirs
+
+def searchPath(userPath): #encontra todos os Processos gerados pelo binário
+    dirs=listDirs()
+    userPathPids=[]
+    for pid in dirs:
+        procsLinks=f"/proc/{pid}/exe"
+        if os.path.exists(procsLinks) and os.path.islink(procsLinks):
+            procPath=os.readlink(procsLinks)
+            if userPath==procPath:
+                userPathPids.append(pid)
+    return userPathPids
+
+def procMetrics(pid):
+    process = psutil.Process(pid)
+    
+    # Porcentagem total de uso de CPU pelo processo
+    cpu_percent = process.cpu_percent(interval=1)
+    
+    # Total de memória usada pelo processo em bytes
+    mem_info = process.memory_info()
+    mem_used = mem_info.rss
+    
+    # Total de atividade de disco pelo processo em bytes
+    disk_usage = process.io_counters()
+    
+    return cpu_percent, mem_used, disk_usage
+
+def showMetrics(pids):
+    for pid in pids:
+        cpu,mem,disk=procMetrics(int(pid))
+        print(pid)
+        print(cpu)#%
+        print(mem)#bits
+        print(disk)
+        print('')
+
+def main():
+    binPath=input("bin: ")
+    binPids=searchPath(binPath)
+    showMetrics(binPids)
+
+main()
